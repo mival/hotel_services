1f39abd7c31f261ecc3492df21fa7781111642f7
Jan Voňavka <jan.vonavka@gmail.com>
2025-02-25 00:04:48 +0100

    init

---
+++ functions/src/index.ts
@@ -0,0 +1,103 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+import { onRequest } from "firebase-functions/v2/https";
+import { onDocumentWritten } from "firebase-functions/v2/firestore";
+import * as logger from "firebase-functions/logger";
+
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://frdudnkvmymiehngjzww.supabase.co";
+const supabaseKey = process.env.SUPABASE_KEY || "";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
+
+export const helloWorld = onRequest(async (request, response) => {
+  const { services = [] } = request.query;
+  console.log(services);
+
+  let query = supabase
+    .from("hotels")
+    .select(
+      "hotel_id, hotels_services!inner(hotel_id, service_id, services:services!inner(name))"
+    );
+
+  (services as String[]).forEach((service) => {
+    query = query.filter("hotels_services.services.name", "eq", service);
+  });
+
+  const { data, error } = await query;
+
+  if (error) {
+    response.status(500).send(error);
+    return;
+  }
+
+  response.send(data.map((hotel: any) => ({ hotel_id: hotel.hotel_id })));
+});
+
+exports.modifyuser = onDocumentWritten("hotels/{hotelId}", async (event) => {
+  if (!event.data) {
+    return;
+  }
+
+  const id = event.params.hotelId;
+  const document = event.data.after.data();
+
+  // delete
+  if (!document) {
+    try {
+      await supabase.from("hotels").delete().match({ hotel_id: id });
+      await supabase.from("hotels_services").delete().match({ hotel_id: id });
+    } catch (error) {
+      logger.error("error", error);
+    }
+    return;
+  }
+
+  try {
+    await supabase.from("hotels").upsert([{ hotel_id: id }]);
+
+    await Promise.all(
+      document.services.map((service: any) => {
+        supabase
+          .from("services")
+          .select("id")
+          .eq("name", service)
+          .then((existingService) => {
+            if (existingService.data?.length) {
+              const existingServiceId = existingService.data[0].id;
+              return supabase
+                .from("hotels_services")
+                .upsert([{ hotel_id: id, service_id: existingServiceId }]);
+            } else {
+              return supabase
+                .from("services")
+                .upsert([{ name: service }])
+                .select("id")
+                .then((response) => {
+                  if (!response.data) {
+                    return;
+                  }
+
+                  return supabase
+                    .from("hotels_services")
+                    .upsert([
+                      { hotel_id: id, service_id: response.data[0].id },
+                    ]);
+                });
+            }
+          });
+      })
+    );
+  } catch (error) {
+    logger.error("error", error);
+  }
+});
