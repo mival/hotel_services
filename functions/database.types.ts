50c009452cde3cf6eef25d6da80c82b4019bf0e5
Jan Voňavka <jan.vonavka@gmail.com>
2025-02-27 00:48:06 +0100

    working

--- functions/database.types.ts
+++ functions/database.types.ts
@@ -1,3 +1,195 @@
-Need to install the following packages:
-supabase@2.12.1
-Ok to proceed? (y) 
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      hotels: {
+        Row: {
+          created_at: string
+          hotel_id: string
+          updated_at: string | null
+        }
+        Insert: {
+          created_at?: string
+          hotel_id: string
+          updated_at?: string | null
+        }
+        Update: {
+          created_at?: string
+          hotel_id?: string
+          updated_at?: string | null
+        }
+        Relationships: []
+      }
+      hotels_services: {
+        Row: {
+          created_at: string
+          hotel_id: string
+          service_id: number
+          updated_at: string | null
+        }
+        Insert: {
+          created_at?: string
+          hotel_id: string
+          service_id: number
+          updated_at?: string | null
+        }
+        Update: {
+          created_at?: string
+          hotel_id?: string
+          service_id?: number
+          updated_at?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "hotels_services_hotel_id_fkey"
+            columns: ["hotel_id"]
+            isOneToOne: false
+            referencedRelation: "hotels"
+            referencedColumns: ["hotel_id"]
+          },
+          {
+            foreignKeyName: "hotels_services_service_id_fkey"
+            columns: ["service_id"]
+            isOneToOne: false
+            referencedRelation: "services"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      services: {
+        Row: {
+          created_at: string
+          id: number
+          name: string | null
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          name?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          name?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+type PublicSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
+        PublicSchema["Views"])
+    ? (PublicSchema["Tables"] &
+        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof PublicSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
+    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof PublicSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
+    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof PublicSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof PublicSchema["CompositeTypes"]
+    ? PublicSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,29 +9,32 @@
 
 import { onRequest } from "firebase-functions/v2/https";
 import { onDocumentWritten } from "firebase-functions/v2/firestore";
+import { Database } from "../database.types";
 import * as logger from "firebase-functions/logger";
 
 import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://frdudnkvmymiehngjzww.supabase.co";
-const supabaseKey = process.env.SUPABASE_KEY || "";
-const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest(async (request, response) => {
+exports.serviceFilter = onRequest(async (request, response) => {
+  if (!process.env.SUPABASE_KEY) {
+    response.status(500).send("SUPABASE_KEY is not set");
+  }
+
+  const supabase = createClient<Database>(
+    supabaseUrl,
+    process.env.SUPABASE_KEY || ""
+  );
   const { services = [] } = request.query;
-  console.log(services);
 
   let query = supabase
     .from("hotels")
     .select(
-      "hotel_id, hotels_services!inner(hotel_id, service_id, services:services!inner(name))"
-    );
-
-  (services as String[]).forEach((service) => {
-    query = query.filter("hotels_services.services.name", "eq", service);
-  });
+      "hotel_id, hotels_services!inner(hotel_id, service_id, services!inner(name))"
+    )
+    .in("hotels_services.services.name", (services as string[]));
 
   const { data, error } = await query;
 
@@ -40,14 +43,40 @@ export const helloWorld = onRequest(async (request, response) => {
     return;
   }
 
-  response.send(data.map((hotel: any) => ({ hotel_id: hotel.hotel_id })));
+  const hotelData = data
+    .map((hotel: any) => {
+      return {
+        hotel_id: hotel.hotel_id,
+        services: hotel.hotels_services.map(
+          (service: any) => service.services.name
+        ),
+      };
+    })
+    .filter((hotel: any) => {
+      return (services as string[]).every((service: string) =>
+        hotel.services.includes(service)
+      );
+    });
+
+  const unique = [...new Set(hotelData.map((hotel: any) => hotel.hotel_id))];
+
+  response.send(unique.map((hotelId: any) => ({ hotel_id: hotelId })));
 });
 
-exports.modifyuser = onDocumentWritten("hotels/{hotelId}", async (event) => {
+exports.hotelChanges = onDocumentWritten("hotels/{hotelId}", async (event) => {
   if (!event.data) {
     return;
   }
 
+  if (!process.env.SUPABASE_KEY) {
+    logger.error("SUPABASE_KEY is not set");
+    return;
+  }
+
+  const supabase = createClient<Database>(
+    supabaseUrl,
+    process.env.SUPABASE_KEY
+  );
   const id = event.params.hotelId;
   const document = event.data.after.data();
 
@@ -63,7 +92,10 @@ exports.modifyuser = onDocumentWritten("hotels/{hotelId}", async (event) => {
   }
 
   try {
-    await supabase.from("hotels").upsert([{ hotel_id: id }]);
+    await supabase
+      .from("hotels")
+      .upsert([{ hotel_id: id, updated_at: new Date().toISOString() }]);
+    await supabase.from("hotels_services").delete().match({ hotel_id: id });
 
     await Promise.all(
       document.services.map((service: any) => {
@@ -71,18 +103,24 @@ exports.modifyuser = onDocumentWritten("hotels/{hotelId}", async (event) => {
           .from("services")
           .select("id")
           .eq("name", service)
-          .then((existingService) => {
+          .then((existingService: any) => {
             if (existingService.data?.length) {
               const existingServiceId = existingService.data[0].id;
               return supabase
                 .from("hotels_services")
-                .upsert([{ hotel_id: id, service_id: existingServiceId }]);
+                .upsert([
+                  {
+                    hotel_id: id,
+                    service_id: existingServiceId,
+                    updated_at: new Date().toISOString(),
+                  },
+                ]);
             } else {
               return supabase
                 .from("services")
-                .upsert([{ name: service }])
+                .insert([{ name: service }])
                 .select("id")
-                .then((response) => {
+                .then((response: any) => {
                   if (!response.data) {
                     return;
                   }
@@ -90,7 +128,11 @@ exports.modifyuser = onDocumentWritten("hotels/{hotelId}", async (event) => {
                   return supabase
                     .from("hotels_services")
                     .upsert([
-                      { hotel_id: id, service_id: response.data[0].id },
+                      {
+                        hotel_id: id,
+                        service_id: response.data[0].id,
+                        updated_at: new Date().toISOString(),
+                      },
                     ]);
                 });
             }
